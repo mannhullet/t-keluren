7e6b61319939c4464e0ade01adcb9d45c1384b7b
leomari <mari.leonhardsen@gmail.com>
2017-03-07 16:44:20 +0100

    Legger vers i separatfil, sjiraffen.js

--- getVerse.js
+++ getVerse.js
@@ -1,17 +1,9 @@
 function getVerse(){
 
-var myObj={"sjiraffenvers":[
-    {"tekst":"Jeg ønsker meg en himmelseng en himmelseng med speil i, så jeg kan ligge og speile meg og se hvor jeg er deilig.",
-    "dato":"15.02.17",
-    "kommentar":"Originalt vers, tatt fra Onlines hjemmeside"},
-    {"tekst":"jeg møtte på en homofil han ville ta meg bakfra men jeg tok å lurte han jeg snudde meg og gapa",
-    "dato":"15.02.17",
-    "kommentar":"Originalt vers, tatt fra Onlines hjemmeside"},
-    {"tekst":"et vers til ",
-    "dato":"15.02.17",
-    "kommentar":"Originalt vers, tatt fra Onlines hjemmeside"}
-            ]};
+
 var myJSON=JSON.stringify(myObj);
+
+
 localStorage.setItem("testJSON", myJSON);
 
 text=localStorage.getItem("testJSON");
