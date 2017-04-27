1891a42c9d0bcdf6cc7be22df14d9f3a9f777be6
Andreas Bro Kolstø <andreas.kolsto@gmail.com>
2017-04-27 11:16:17 +1000

    Sjekk om versinfo er undefined før den vises
    Hindrer at vers som har manglende felt skriver ut "undefined" i testfeltet.
    Burde ikke være et problem for vers som blir automatisk lagt til, men
    greit å ha uansett.

--- getVerse.js
+++ getVerse.js
@@ -9,14 +9,26 @@ function getVerse() {
           alle har blitt vist minst 1 gang */
   var i = Math.floor((Math.random() * antallVers));
 
-  /*TODO: Sjekk at verset faktisk har en property før den vises. Feks hvis
-          det ikke er noen forfatter lagt inn, så skal det vises ingenting
-          i stedet for "undefined". */
   document.getElementById("plassholder").innerHTML = "<span class=\"infoDescription\"> Trykk igjen for nytt vers! </span>";
   document.getElementById("visVers").innerHTML = obj.sjiraffenvers[i].tekst;
-  document.getElementById("visForfatter").innerHTML = "<span class=\"infoDescription\">Forfatter: </span>" + obj.sjiraffenvers[i].forfatter;
-  document.getElementById("visKommentar").innerHTML = "<span class=\"infoDescription\">Kommentar: </span>" + obj.sjiraffenvers[i].kommentar;
-  document.getElementById("visDato").innerHTML = "<span class=\"infoDescription\">Skrevet: </span>" + obj.sjiraffenvers[i].dato;
+
+  var forfatter = obj.sjiraffenvers[i].forfatter;
+  var kommentar = obj.sjiraffenvers[i].kommentar;
+  var dato = obj.sjiraffenvers[i].dato;
+
+  if (forfatter === undefined) {
+    forfatter = "Ukjent";
+  }
+  if (kommentar === undefined) {
+    kommentar = "";
+  }
+  if (dato === undefined) {
+    dato = "Ukjent";
+  }
+
+  document.getElementById("visForfatter").innerHTML = "<span class=\"infoDescription\">Forfatter: </span>" + forfatter;
+  document.getElementById("visKommentar").innerHTML = "<span class=\"infoDescription\">Kommentar: </span>" + kommentar;
+  document.getElementById("visDato").innerHTML = "<span class=\"infoDescription\">Skrevet: </span>" + dato;
 
 
 }
